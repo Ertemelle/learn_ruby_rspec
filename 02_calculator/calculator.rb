6241768bd61bca49d23eb9707cf58bddb788b1e0
Ertemelle <phenixime@hotmail.fr>
2018-07-09 14:28:28 +0200

    fin exo_02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -6,14 +6,22 @@ def subtract (x, y)
 	return x - y
 end
 
-def sum
+def sum (array)
+	sum = 0
+	array.each do |i|
+		sum += i
+	end
+	return sum
 end
 
-def multiply
+def multiply(x, y)
+	return x * y
 end
 
-def power
+def power(x, y)
+	return x ** y
 end
 
-def factorial
+def factorial(x)
+	return Math.gamma(x+1)
 end
